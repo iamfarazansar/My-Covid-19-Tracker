b23cf2036ecd9d694f1271736715aaf7dcd939ad
iamfarazansar <iamfarazansar@gmail.com>
2020-09-08 01:28:32 +0530

    Updates

---
+++ precache-manifest.7f5d88575603d531e8a6c42cd10e0f2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9af549707210366c59e2c112d3f2d62",
+    "url": "/My-Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "c1a430c9ef7b14ff2bc8",
+    "url": "/My-Covid-19-Tracker/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "0f11c20fdd0839c475c5",
+    "url": "/My-Covid-19-Tracker/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "c1a430c9ef7b14ff2bc8",
+    "url": "/My-Covid-19-Tracker/static/js/2.7ec581d5.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/My-Covid-19-Tracker/static/js/2.7ec581d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f11c20fdd0839c475c5",
+    "url": "/My-Covid-19-Tracker/static/js/main.7dec5536.chunk.js"
+  },
+  {
+    "revision": "d0f2887ad75ec5f84498",
+    "url": "/My-Covid-19-Tracker/static/js/runtime-main.23b22cc2.js"
+  }
+]);
